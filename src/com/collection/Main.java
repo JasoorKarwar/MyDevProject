b07a31c762cbc31d363a7ed5a1f935767db23195
JasoorKarwar <karwar.jasoor@gmail.com>
2021-03-06 23:16:12 -0500

    As a new developer, this is my first commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i=0; i<5; i++){
             System.out.println("Hello batch 8");
+            System.out.println("Test");
             Thread.sleep(2000);
         }
     }
